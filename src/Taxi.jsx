2d7b312e4b7d3edea86635115e157e556664e5d9
Seungmin Ham <hamsm3184@naver.com>
2023-06-24 00:40:43 +0900

    '6/24'

--- src/Taxi.jsx
+++ src/Taxi.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import style from './Bus.module.css';
+import style from './Taxi.module.css';
+
+
 
 function Taxi() {
+
+    const text = {
+        0: '✅ 반드시 도봉산 역에 도착한 후 신청해 주세요. 매칭 후에는 취소할 수 없어요!. 신중히 눌러주세요! \n ✅ '
+    }
+
     return (
         <div>
-            <textarea readOnly className={style.agree}>ddddd</textarea>
+            <h2>⚠ 필독 ⚠</h2>
+            <textarea readOnly className={style.agree}>{text[0]}</textarea>
+            <br></br>
+            <p className='agree'>동의합니다. <input  className={style.check} type="checkbox" name="xxx" value="yyy" ></input> </p>
+           
 
         </div>
     )
