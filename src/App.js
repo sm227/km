ac70373042db19397a92e90399b67a4c1db7cc2d
Seungmin Ham <hamsm3184@naver.com>
2023-06-23 15:19:08 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import Menu from './Menu';
+
 
 function App() {
+  const movePage = useNavigate()
+
+  function kmPortal() {
+      window.open('https://portal.kyungmin.ac.kr/portal/index.do')
+  }
+
+  function goMenu() {
+    movePage('./Menu');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+
+        <BrowserRouter>
+          <Routes>
+              <Route path={"./Menu"} element={<Menu/>}></Route>
+              
+          </Routes>
+          </BrowserRouter>
+
+     <h1 className="title">오뱃뭐?</h1>
+     <h4 className= "subTitle">환영합니다.</h4>
+     <p><button className="glow-on-hover" type="button" onClick={goMenu}>🍚 오늘의 메뉴 확인하러 가기</button></p>
+     <p><button className="glow-on-hover" type="button">🚕 택시 이용하기</button></p>
+     <p><button className="glow-on-hover" type="button" onClick = {kmPortal}>🏫 경민대학교 포털</button></p>
+     <br></br>
+     <h3 className = "ver">Ver 2.0 Beta</h3>
     </div>
-  );
+
+
+  )
 }
 
 export default App;
---
+++ src/Menu.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+
+function Menu() {
+    return (
+        <div> <h1>오늘의 메뉴는</h1></div>
+    )
+}
+
+export default Menu;
